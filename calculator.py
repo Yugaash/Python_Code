4f03785ceba8ebf4886b44f4b442a837dbc0a8c6
Yugaash S <166541060+Yugaash@users.noreply.github.com>
2024-04-11 12:04:25 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,41 @@
+def Add(val1, val2):
+    return val1 + val2
+
+
+def Subtract(val1, val2):
+    return val1 - val2
+
+
+def Multiply(val1, val2):
+    return val1 * val2
+
+
+def Divide(val1, val2):
+    return val1 / val2
+
+
+print("Select:")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiply")
+print("d. Divide")
+
+proceed = input("Select Choice a, b, c, d: ")
+
+Val1 = float(input("Num1: "))
+Val2 = float(input("Num2: "))
+
+if proceed == 'a':
+    print(Val1, "+", Val2, "=", Add(Val1, Val2))
+
+elif proceed == 'b':
+    print(Val1, "-", Val2, "=", Subtract(Val1, Val2))
+
+elif proceed == 'c':
+    print(Val1, "*", Val2, "=", Multiply(Val1, Val2))
+
+elif proceed == 'd':
+    print(Val1, "/", Val2, "=", Divide(Val1, Val2))
+
+else:
+    print("Enter valid choice")
